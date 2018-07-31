1cbb67f787371a71f0e64f509b1b7d09e0174b6a
Anna Bogdanova <ann.bogdanova@gmail.com>
2018-08-01 08:59:50 +0900

    extended global class deffinition

--- SwarmPackagePy/intelligence.py
+++ SwarmPackagePy/intelligence.py
@@ -4,6 +4,7 @@ class sw(object):
 
         self.__Positions = []
         self.__Gbest = []
+        self.__Nests = []
 
     def _set_Gbest(self, Gbest):
         self.__Gbest = Gbest
@@ -21,3 +22,46 @@ class sw(object):
         """Return the best position of algorithm (return type: list)"""
 
         return list(self.__Gbest)
+
+    def _newbee(self, l, c, lb, ub):
+
+        bee = []
+        for i in l:
+            new = [self.__neighbor(i, lb, ub) for k in range(c)]
+            bee += new
+        bee += l
+
+        return bee
+
+    def _neighbor(self, who, lb, ub):
+
+        neighbor = np.array(who) + uniform(-1, 1) * (
+            np.array(who) - np.array(
+                self.__agents[randint(0, len(self.__agents) - 1)]))
+        neighbor = np.clip(neighbor, lb, ub)
+
+        return list(neighbor)
+
+    def _nest(self):
+        self.__Nests.append([list(i) for i in self.__nests])
+
+    def _Levyfly(self, Pbest, n, dimension):
+
+        beta = 3 / 2
+        sigma = (gamma(1 + beta) * sin(pi * beta / 2) / (
+            gamma((1 + beta) / 2) * beta *
+            2 ** ((beta - 1) / 2))) ** (1 / beta)
+        u = np.array([normalvariate(0, 1) for k in range(dimension)]) * sigma
+        v = np.array([normalvariate(0, 1) for k in range(dimension)])
+        step = u / abs(v) ** (1 / beta)
+
+        for i in range(n):
+            stepsize = 0.2 * step * (self.__agents[i] - Pbest)
+            self.__agents[i] += stepsize * np.array([normalvariate(0, 1)
+                                                    for k in range(dimension)])
+
+    def get_nests(self):
+        """Return a history of cuckoos nests (return type: list)"""
+
+        return self.__Nests
+
--- example.py
+++ example.py
@@ -3,8 +3,7 @@ from SwarmPackagePy import testFunctions as tf
 from SwarmPackagePy import animation, animation3D
 
 
-alh = SwarmPackagePy.pso(50, tf.easom_function, -10, 10, 2, 20,
-                         w=0.5, c1=1, c2=1)
-animation(alh.get_agents(), tf.easom_function, -10, 10)
-animation3D(alh.get_agents(), tf.easom_function, -10, 10)
+alh = SwarmPackagePy.pso(50, tf.ackley_function, -10, 10, 2, 20)
+animation(alh.get_agents(), tf.ackley_function, -10, 10)
+animation3D(alh.get_agents(), tf.ackley_function, -10, 10)
 
